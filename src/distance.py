7f1e5fff862d4faffbb7180fc2c367fde898a791
Ricardo Serra <rserra@surfncode.io>
2019-07-13 11:48:38 +0100

    Adds distance_between_points method and respective unit tests. Removes degrees_to_radians because math module already provides a conversion method named 'radians'

--- src/distance.py
+++ src/distance.py
@@ -1,11 +1,39 @@
-from math import pi
+from math import acos, cos, radians, sin
 
 
-def degrees_to_radians(degrees):
-    """ Converts Degrees to Radians: https://en.wikipedia.org/wiki/Radian#Conversion_between_radians_and_degrees
+# Earth Radius in Kilometers: https://en.wikipedia.org/wiki/Earth_radius
+EARTH_RADIUS_KMS = 6371
+
+
+def distance_between_points(lat1, long1, lat2, long2):
+    """ Returns distance between point 1 with 'lat1','long1' and point 2 with
+    'lat2','long2' https://en.wikipedia.org/wiki/Great-circle_distance#Formulae
+
     Parameters:
-        degrees (int): Degrees to be converted
+        lat1 (float): Latitude, in degrees, from the point 1
+        long1 (float): Longitude, in degrees, from the point 1
+        lat2 (float): Latitude, in degrees, from point 2
+        long2 (float): Longitude, in degrees, from the point 2
     Returns:
-        radians (float): Degrees 'degrees' converted to Radians
+        distance (float): Distance, between point 1 and point 2 in KMs
     """
-    return degrees * pi / 180
+    lat1 = float(lat1)
+    lat2 = float(lat2)
+    long1 = float(long1)
+    long2 = float(long2)
+
+
+    # Convert degrees to radians
+    long1 = radians(long1)
+    long2 = radians(long2)
+    lat1 = radians(lat1)
+    lat2 = radians(lat2)
+
+    # Delta between longitudes
+    dlong = long2 - long1
+
+    # Central angle between point 1 and point 2
+    c = acos((sin(lat1)*sin(lat2) + cos(lat1) * cos(lat2) * cos(dlong)))
+
+    # Distance, in KMs, is central angle * Earth radius
+    return c * EARTH_RADIUS_KMS
--- test/test_distance.py
+++ test/test_distance.py
@@ -1,22 +1,48 @@
-from pytest import approx
+from pytest import approx, raises
 
-from src.distance import degrees_to_radians
+from src.distance import distance_between_points
 
 # Floating Point tolerance to be used in unit tests
 FLOATING_POINT_TOLERANCE = 0.001
 
 
-def test_100_degree_to_radians():
-    assert degrees_to_radians(100) == approx(1.745, rel=FLOATING_POINT_TOLERANCE)
+def test_distance_between_tower_brigde_and_eiffel_tower():
+    tower_bridge = (51.503378, -0.076579)
+    eiffel_tower = (48.8583, 2.294499999999971)
+    distance = distance_between_points(
+        tower_bridge[0], tower_bridge[1], eiffel_tower[0], eiffel_tower[1])
+    assert distance == approx(339.089, rel=FLOATING_POINT_TOLERANCE)
 
 
-def test_2_degree_to_radians():
-    assert degrees_to_radians(2) == approx(0.0349, rel=FLOATING_POINT_TOLERANCE)
+def test_distance_between_dublin_office_temple_bar():
+    dublin_office = (53.339428, -6.257664)
+    temple_bar = (53.345602, -6.262875)
+    distance = distance_between_points(
+        dublin_office[0], dublin_office[1],
+        temple_bar[0], temple_bar[1])
+    assert distance == approx(0.768, rel=FLOATING_POINT_TOLERANCE)
 
 
-def test_180_degree_to_radians():
-    assert degrees_to_radians(180) == approx(3.141, rel=FLOATING_POINT_TOLERANCE)
+def test_distance_between_dublin_office_temple_bar_using_strings():
+    dublin_office = ("53.339428", "-6.257664")
+    temple_bar = ("53.345602", "-6.262875")
+    distance = distance_between_points(
+        dublin_office[0], dublin_office[1],
+        temple_bar[0], temple_bar[1])
+    assert distance == approx(0.768, rel=FLOATING_POINT_TOLERANCE)
 
 
-def test_minus_180_degree_to_radians():
-    assert degrees_to_radians(-180) == approx(-3.141, rel=FLOATING_POINT_TOLERANCE)
+def test_distance_between_same_location():
+    random_location = (51.12233, 0.881233)
+    distance = distance_between_points(
+        random_location[0], random_location[1],
+        random_location[0], random_location[1])
+    assert distance == approx(0.000, rel=FLOATING_POINT_TOLERANCE)
+
+
+def test_distance_using_lists():
+    random_location = ([51.12233], [0.881233])
+    with raises(TypeError):
+        assert distance_between_points(
+            random_location[0], random_location[1],
+            random_location[0], random_location[1])
